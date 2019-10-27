573454e7dab4d64bd177b154a7bdbf9e3cb08b95
Dai, Pengcheng <dpc_work@163.com>
2019-10-27 21:28:02 +0800

    Add library path.

--- config/init.sh
+++ config/init.sh
@@ -26,9 +26,18 @@ done
 PATH=$HOME/.local/bin:$PATH
 LIBRARY_PATH=$HOME/.local/lib:$LIBRARY_PATH
 LD_LIBRARY_PATH=$HOME/.local/lib:$LD_LIBRARY_PATH
+C_INCLUDE_PATH=$HOME/.local/include:$C_INCLUDE_PATH
+CPLUS_INCLUDE_PATH=$HOME/.local/include:$CPLUS_INCLUDE_PATH
+for dir in $(find "$HOME/.local/include" -mindepth 1 -maxdepth 1 -type d)
+do
+    C_INCLUDE_PATH="$dir:$C_INCLUDE_PATH"
+    CPLUS_INCLUDE_PATH="$dir:$CPLUS_INCLUDE_PATH"
+done
 
 export LIBRARY_PATH="$(trim $LIBRARY_PATH ":")"
 export LD_LIBRARY_PATH="$(trim $LD_LIBRARY_PATH ":")"
+export C_INCLUDE_PATH="$(trim $C_INCLUDE_PATH ":")"
+export CPLUS_INCLUDE_PATH="$(trim $CPLUS_INCLUDE_PATH ":")"
 
 # source all config file
 CONFIGDIR="$DOTFILES_ROOT/config"
