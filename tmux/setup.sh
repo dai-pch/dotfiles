140eab41be7e6308e7a7a1c87d5bb396b36f307a
Dai, Pengcheng <dpc_work@163.com>
2019-10-29 17:00:40 +0800

    Add tmux config.

---
+++ tmux/setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+source "$(dirname "$BASH_SOURCE[0]")/../common.sh"
+
+# depends on add_to_file function
+source $DOTFILES_ROOT/functions/add_to_file.sh
+CONF_FILE="tmux/tmux.conf"
+
+# setup bashrc
+add_to_file "$HOME/.tmux.conf" "##### Added by dotfiles bootstrap #####\nsource-file $DOTFILES_ROOT/$CONF_FILE"
+
