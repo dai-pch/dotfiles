eca5e67a82db5371abacf9b31452ff567ac3b974
Dai, Pengcheng <dpc_work@163.com>
2020-05-05 19:54:41 +0800

    add vim-plug install.

--- vim/setup.sh
+++ vim/setup.sh
@@ -5,6 +5,10 @@ source "$(dirname "$BASH_SOURCE[0]")/../common.sh"
 source $DOTFILES_ROOT/functions/add_to_file.sh
 VIM_FILE="vim/main.vim"
 
+# install vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 # setup bashrc
 add_to_file "$HOME/.vimrc" "\"##### Added by dotfiles bootstrap #####\nsource $DOTFILES_ROOT/$VIM_FILE"
 
