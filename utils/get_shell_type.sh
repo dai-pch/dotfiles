43747f3d414b426a55c86f13873104a58a44142c
Dai, Pengcheng <dpc_work@163.com>
2020-05-06 19:47:50 +0800

    Fix bugs.

--- utils/get_shell_type.sh
+++ utils/get_shell_type.sh
@@ -4,10 +4,9 @@ get_shell_type() {
     local shell_path
 
     shell_path=$SHELL
-    prompt=$(sudo -nv 2>&1)
-    if [ -n $shell_path -c "echo \$ZSH_VERSION" ]; then
+    if [ -n $($shell_path -c "echo \$ZSH_VERSION") ]; then
         echo "zsh"
-    elif [ -n $shell_path -c "echo \$BASH_VERSION" ]; then
+    elif [ -n $($shell_path -c "echo \$BASH_VERSION") ]; then
         echo "bash"
     else
         echo "others"
