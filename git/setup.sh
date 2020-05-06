14f9257ed3cf74b52d9d286c03c0e8dda726e31d
Dai, Pengcheng <dpc_work@163.com>
2020-05-06 14:48:11 +0800

    Remove auto set git config.

--- git/setup.sh
+++ git/setup.sh
@@ -2,12 +2,12 @@
 
 GIT_CMD="$(which git)"
 
-if [ ! -z "$GIT_CMD" ]; then
-    if [ -z "$(git config --get user.email)" ]; then
-        git config --global user.email="dpc_work@163.com"
-    fi
-    if [ -z "$(git config --get user.name)" ]; then
-        git config --global user.name="Dai, Pengcheng"
-    fi
-fi
+# if [ ! -z "$GIT_CMD" ]; then
+#     if [ -z "$(git config --get user.email)" ]; then
+#         git config --global user.email="dpc_work@163.com"
+#     fi
+#     if [ -z "$(git config --get user.name)" ]; then
+#         git config --global user.name="Dai, Pengcheng"
+#     fi
+# fi
 
