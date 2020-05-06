7382719d3f63d1601b88857eb594a307b53564f5
Dai, Pengcheng <dpc_work@163.com>
2020-05-06 20:53:29 +0800

    Fix compatibilty for common.sh on zsh.

--- common.sh
+++ common.sh
@@ -1,2 +1,6 @@
-DOTFILES_ROOT="$(cd "$(dirname "$BASH_SOURCE[0]")" && pwd -P)"
+if [ ! -z $BASH_VERSION ]; then
+    DOTFILES_ROOT="$(cd "$(dirname "$BASH_SOURCE[0]")" && pwd -P)"
+elif [ ! -z $ZSH_VERSION ]; then
+    DOTFILES_ROOT="$(cd "$(dirname "${(%):-%N}")" && pwd -P)"
+fi
 
